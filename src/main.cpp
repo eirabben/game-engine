ef952ff5a2b42ab4ce2578bb514201e4a9db00d3
Eirik Igland <eirabben@gmail.com>
2016-02-05 23:01:10 +0100

    Add build dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include "Game.hpp"
+/* #include "Game.hpp" */
 
-int main(int argc, char* args[]) {
-    Game game;
-    game.run();
+/* int main(int argc, char* args[]) { */
+/*     Game game; */
+/*     game.run(); */
 
-    return 0;
-}
+/*     return 0; */
+/* } */
